7bc28821cc6133466715876a3fd7133fa29f4036
Andrey Zhirkov <zhirkov.arty@gmail.com>
2023-07-18 20:47:18 +0300

    add function subtraction

--- py_project/funcs.py
+++ py_project/funcs.py
@@ -1,3 +1,11 @@
 def get_sum(num_one, num_two):
     result = num_one + num_two
     return result
+
+
+def subtraction(num_one, num_two):
+    result = num_one - num_two
+    return result
+
+
+print(subtraction(2, 1))
